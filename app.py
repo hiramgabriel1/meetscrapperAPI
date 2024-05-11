2e5b6036fba701e0553fe9a6cfa414cd4d20c75d
@hiram.dev <82051017+itsrusty@users.noreply.github.com>
2024-05-11 10:40:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
 import instaloader
 
-def scrape_instagram_profile(username):
-    # Crea una instancia de Instaloader
-    L = instaloader.Instaloader()
+loader = instaloader.Instaloader()
+username = ''
 
-    try:
-        # Obtén el perfil de Instagram
-        profile = instaloader.Profile.from_username(L.context, username)
+try:
+    profile = instaloader.Profile.from_username(loader.context, username)
 
-        # Imprime la información del perfil
-        print(f"Username: {profile.username}")
-        print(f"Biografía: {profile.biography}")
-        print(f"Seguidores: {profile.followers}")
-        print(f"Siguiendo: {profile.followees}")
+    print("Nombre de usuario:", profile.username)
+    print("Biografía:", profile.biography)
+    print("Seguidores:", profile.followers)
+    print("Siguiendo:", profile.followees)
 
-        # Imprime las descripciones, etiquetados y hashtags de las publicaciones
-        print("Publicaciones:")
-        for post in profile.get_posts():
-            print("- Descripción:", post.caption)
-            print("- Etiquetados:", [tagged_user.username for tagged_user in post.tagged_users])
-            print("- Hashtags:", [hashtag for hashtag in post.caption_hashtags])
+    print("Información de las publicaciones:")
+    print("Información de las publicaciones:")
+    for post in profile.get_posts():
+        print("Descripción:", post.caption)
+        print("Hashtags:", post.caption_hashtags)
+        print("Usuarios etiquetados:", post.tagged_users)
 
-    except Exception as e:
-        print(f"Error al obtener el perfil de Instagram: {e}")
+        for tag in post.tagged_users:
+            print(tag)
 
-# Solicitar al usuario el nombre de usuario del perfil que desea scrapear
-username_to_scrape = input("Ingrese el nombre de usuario del perfil que desea scrapear: ")
-
-# Llama a la función de scrapping con el nombre de usuario especificado
-scrape_instagram_profile(username_to_scrape)
+except instaloader.exceptions.ProfileNotExistsException:
+    print("El perfil especificado no existe.")
+except instaloader.exceptions.ConnectionException:
+    print("Error de conexión. Por favor, verifica tu conexión a internet.")
