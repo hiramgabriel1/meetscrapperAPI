9da061336b796f19001f3947bf5d7d9cded24f00
itsrusty <rusty3031@gmail.com>
2024-05-12 17:18:17 -0600

    feature: send data to my API

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
 import instaloader
+import requests
+
+API_URL = 'http://localhost:5000/data-analyzer'
 
 loader = instaloader.Instaloader()
-username = ''
+username = 'hiram.dev'
 
 try:
     profile = instaloader.Profile.from_username(loader.context, username)
 
-    print("Nombre de usuario:", profile.username)
-    print("Biografía:", profile.biography)
-    print("Seguidores:", profile.followers)
-    print("Siguiendo:", profile.followees)
+    data_collected = {
+        "nombrePersona": profile.username,
+        "biografiaPersona": profile.biography,
+        "seguidores": profile.followers,
+        "siguiendo": profile.followees,
+        "publicaciones": []
+    }
 
-    print("Información de las publicaciones:")
-    print("Información de las publicaciones:")
     for post in profile.get_posts():
-        print("Descripción:", post.caption)
-        print("Hashtags:", post.caption_hashtags)
-        print("Usuarios etiquetados:", post.tagged_users)
+        post_data = {
+            "descripcionPost": post.caption,
+            "hashtags": post.caption_hashtags,
+            "amigosMencionados": [loader.check_profile_id(tag).username for tag in post.tagged_users]
+        }
+        data_collected["publicaciones"].append(post_data)
+    
+    response = requests.post(API_URL, json=data_collected)
 
-        for tag in post.tagged_users:
-            print(tag)
+    if response.status_code == 200:
+        print("Datos enviados exitosamente a la API.")
+    else:
+        print("Error al enviar los datos a la API:", response.text)
 
 except instaloader.exceptions.ProfileNotExistsException:
     print("El perfil especificado no existe.")
